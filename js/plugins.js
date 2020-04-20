77bffae62c0eabbfc950b72f2c777c4de7d7db8f
Eslam <solomkhder@gmail.com>
2020-04-20 17:24:20 +0200

    Life Makers System

---
+++ js/plugins.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+    "use strict";
+    $('.carousel').carousel({
+        interval: 500000
+    });
+});
+/*
+var elements = document.getElementsByClassName("dot");
+var i = 0;
+for (i = 0; i < elements.length; i++) {
+    var text = elements[i].innerHTML;
+    elements[i].innerHTML = "<i class=\"fas fa-circle\"></i>" + text;
+}*/
